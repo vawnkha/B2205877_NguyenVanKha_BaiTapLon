dbec2cd3213ec5d4b01bd07b2853609c49f1dd12
vawnkha <nguyen4725kha@gmail.com>
2025-12-02 23:59:03 +0700

    update app.js

--- project-frontend/src/App.vue
+++ project-frontend/src/App.vue
@@ -1,14 +1,11 @@
+<template>
+  <router-view />
+</template>
+
 <script>
 export default {};
 </script>
 
-<template>
-  <h1>Hello, Vue.js!</h1>
-</template>
-
 <style>
-.page {
-  max-width: 400px;
-  margin: auto;
-}
+@import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css";
 </style>
